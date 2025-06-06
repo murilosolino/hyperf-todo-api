04f0745197b192a1925c720de35954bffb481546
murilosolino <murilo.solino@outlook.com>
2025-06-06 20:28:44 -0300

    feat: implementa validação de dados nos métodos store e update de tarefas

--- app/Controller/TaskController.php
+++ app/Controller/TaskController.php
@@ -6,12 +6,18 @@ use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 use App\Service\TaskService;
+use Hyperf\Validation\Contract\ValidatorFactoryInterface;
+use Hyperf\Validation\ValidationException;
+
 
 class TaskController
 {
 
     #[Inject]
     protected TaskService $taskService;
+
+    #[Inject]
+    protected ValidatorFactoryInterface $validatorFactory;
 
     public function index(ResponseInterface $response)
     {
@@ -29,6 +35,23 @@ class TaskController
 
     public function store(RequestInterface $request, ResponseInterface $response)
     {
+
+        $validator = $this->validatorFactory->make(
+            $request->all(),
+            [
+                'title' => 'required|filled|string|min:1',
+                'description' => 'required|filled|string|min:1',
+            ],
+            [
+                'title.required' =>  'Defina um :attribute para a criação da tarefa',
+                'description.required' => 'Impossivel criar terefa sem :attribute',
+            ]
+        );
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
         $data = [
             'title' => $request->input('title'),
             'description' => $request->input('description'),
@@ -44,6 +67,27 @@ class TaskController
 
     public function update(RequestInterface $request, ResponseInterface $response, int $id)
     {
+
+        $validator = $this->validatorFactory->make(
+            $request->all(),
+            [
+                'title' => 'filled|string|min:1',
+                'description' => 'filled|string|min:1',
+                'is_done' => 'integer|between:0,1'
+            ],
+            [
+                'string' => ':attribute deve ser do tipo string',
+                'integer' => ':attribute deve ser do tipo integer',
+                'between' => ':attribute deve ser um integer e não deve ser diferente de 0 ou 1',
+                'min:' => ':attribute deve conter ao menos um caracter',
+                'filled' => ':attribute não pode ser vazio'
+            ]
+        );
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
 
         $data = [
             'title' => $request->input('title'),
